38764ab2cf570a56b777a6f0af8d9e55787a3489
Gowtham8393 <138091988+Gowtham8393@users.noreply.github.com>
2025-07-25 18:49:52 +0530

    Update Country.sql

--- Country.sql
+++ Country.sql
@@ -1,3 +1,5 @@
+--- to select cricket team and play against each country once 
+
 SELECT 
     c1.country AS team_a,
     c2.country as team_b
@@ -8,3 +10,16 @@ JOIN
     ON c1.country < c2.country
 ORDER BY 
     team_a, team_b;
+
+create or replace table "DBT_TGT"."TGT"."countries"
+(country varchar(200));
+select country as team_a from dbt_tgt.tgt."countries";
+
+INSERT INTO "DBT_TGT"."TGT"."countries"  
+VALUES 
+('India'),
+('Pakistan'),
+('Srilanka'),
+('Japan'),
+('Australia'),
+('America');
